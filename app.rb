ed1cbdd46f53ff07e555730fed3fe680c8bc91bf
Johnathon Wood <jaywood128@gmail.com>
2019-02-07 17:00:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,6 +15,5 @@ class App < Sinatra::Base
     @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
     erb :friends
     # Write your code here!
-
   end
 end
